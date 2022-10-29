e1923278972d636c80c68c3a71774d78bae8ddd6
Sergey <sergeygolubev@xakep.ru>
2022-10-30 01:59:48 +0300

    upgraded to React Hooks

--- src/components/todolist/todolist.js
+++ src/components/todolist/todolist.js
@@ -1,47 +1,48 @@
-import React from "react";
+import React, { useState } from 'react';
 import AddButton from "../button/button";
 import InputText from "../input/input";
 import NumberList from "../number-list/number-list";
 
-class TodoList extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { list: [], value: "", counter: -1 };
-  }
-  addText = (event) => {
-    this.setState({ counter: ++this.state.counter });
+const TodoList = () => {
+
+  const [list, setList] = useState([]);
+  const [value, setValue] = useState('');
+  const [counter, setCounter] = useState(0)
+
+  const addText = (event) => {
+    setCounter(counter + 1);
     const item = {
-      id: this.state.counter,
-      text: this.state.value,
-      completed: false,
+      id: counter,
+      text: value,
+      completed: false
     };
-    this.setState({ list: this.state.list.concat(item) });
+    setList(list.concat(item));
   };
-  handleChange = (event) => {
-    this.setState({ value: event.target.value });
+  const handleChange = (event) => {
+    setValue(event.target.value);
   };
-  handleSubmit = (event) => {
+  const handleSubmit = (event) => {
     event.preventDefault();
   };
-  handleSwitch = (event) => {
-    let listCopy = this.state.list;
-    listCopy[event.target.id].completed = !listCopy[event.target.id]
-      .completed;
-    this.setState({ list: listCopy });
+  const handleSwitch = (event) => {
+    let listCopy = [...list];
+    //console.log(list);
+    listCopy[event.target.id].completed = !listCopy[event.target.id].completed;
+    setList(listCopy);
   };
-  render() {
+
     return (
       <ul>
-        <NumberList list={this.state.list} handler={this.handleSwitch} />
+        <NumberList list={list} handler={handleSwitch} />
         <div className="input-group mb-3">
-          <InputText handler={this.handleChange} value={this.state.value} />
+          <InputText handler={handleChange} value={value} />
           <div className="input-group-append">
-            <AddButton handler={this.addText} />
+            <AddButton handler={addText} />
           </div>
         </div>
       </ul>
     );
-  }
+  
 }
 
 export default TodoList;
